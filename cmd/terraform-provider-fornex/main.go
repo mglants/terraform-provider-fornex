3c8b4aa58b5d86ca66c54657bea4104b17c42903
Mark Glants <mark@glants.xyz>
2026-02-06 03:55:01 +0300

    fix: ci

---
+++ cmd/terraform-provider-fornex/main.go
@@ -0,0 +1,34 @@
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/mglants/terraform-provider-fornex/internal/provider"
+)
+
+var (
+	version string = "dev"
+)
+
+func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		Address: "registry.terraform.io/mglants/fornex",
+		Debug:   debug,
+	}
+
+	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
